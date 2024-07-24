1213c72cca149b6a41065445c8dae12c624ae583
Matheus <matheus.oliveirag@outlook.com.br>
2024-07-24 13:26:07 -0300

    testando código

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+const linhas = $("#lista-tarefas");
+$(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDefault();
-    
+
+        const nomeTarefa = $("#nome-tarefa").val();
+        const linha = $(`<li><span>${nomeTarefa}</span></li>`);
+        linhas.append(linha);
+        
+        $("#nome-tarefa").val('');
+
+        $('#lista-tarefas').on('click', 'li', function(e) {
+            if (!$(e.target).is('button')) {
+                $(this).find('span').toggleClass('line-through');
+            }
+        });
     });
-
-
+});
