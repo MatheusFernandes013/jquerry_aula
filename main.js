cef477f3314028d579d081a0d70b2a5c3a0dcb10
Matheus <matheus.oliveirag@outlook.com.br>
2024-07-26 13:07:34 -0300

    finalizando a atividade

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const linhas = $("#lista-tarefas");
 $(document).ready(function(){
-
     $('form').on('submit', function(e){
         e.preventDefault();
 
         const nomeTarefa = $("#nome-tarefa").val();
-        const linha = $(`<li><span>${nomeTarefa}</span></li>`);
-        linhas.append(linha);
-        
-        $("#nome-tarefa").val('');
 
-        $('#lista-tarefas').on('click', 'li', function(e) {
-            if (!$(e.target).is('button')) {
-                $(this).find('span').toggleClass('line-through');
-            }
-        });
+        if (nomeTarefa) {
+            const linha = $(`<li>${nomeTarefa}</li>`);
+            linha.appendTo('ul');
+        
+            $("#nome-tarefa").val('');
+        }
+    });
+    $('#lista-tarefas').on('click', 'li', function() {
+        $(this).toggleClass('line-through');
     });
 });
